1c1275ad0d60e5c485b0fd089b1a34793773d203
mohammad kamruzzaman <romon.zaman@gmail.com>
2023-07-22 15:38:30 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+    import ImageCarousel from './components/ImageCarousel.vue'
+
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="">
+
+    <div class="m-5 p-5 text-center">
+        <span class="text-3xl">Image carousel</span>
+    </div>
+
+    <ImageCarousel />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="">    
 </style>
---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,109 @@
+<script setup>
+
+import { ref, reactive } from 'vue'
+const images = reactive([
+    {
+        id: 1,
+        ImageSmallUrl: '/img/picture1.jpeg',
+        imageLargeUrl: '/img/picture1_large.jpeg'
+    },
+    {
+        id: 2,
+        ImageSmallUrl: '/img/picture2.jpeg',
+        imageLargeUrl: '/img/picture2_large.jpeg'
+    },
+    {
+        id: 3,
+        ImageSmallUrl: '/img/picture3.jpeg',
+        imageLargeUrl: '/img/picture3_large.jpeg'
+    },
+    {
+        id: 4,
+        ImageSmallUrl: '/img/picture4.jpeg',
+        imageLargeUrl: '/img/picture4_large.jpeg'
+    }
+])
+const currentSlide = ref(0)
+
+const nextSlide = () => {
+    currentSlide.value++
+    if (currentSlide.value >= images.length)
+        currentSlide.value = 0
+
+    bgUrl.value = images[currentSlide.value].ImageSmallUrl
+}
+
+const prevSlide = () => {
+    if (currentSlide.value == 0)
+        currentSlide.value = images.length - 1
+    else
+        currentSlide.value--
+    bgUrl.value = images[currentSlide.value].ImageSmallUrl
+}
+
+const updateImage = () => {
+    bgUrl.value = images[currentSlide.value].ImageSmallUrl
+}
+
+let timerPause = 0
+const timerFunc = () => {
+    timerPause = 0
+    setTimeout(() => {
+        if (!timerPause)
+            nextSlide()
+        timerFunc()
+    }, 5000)
+}
+
+timerFunc()
+
+const bgUrl = ref(images[0].ImageSmallUrl)
+
+</script>
+
+<template>
+    <div class="relative m-5 bg-gray-50">
+        <!-- image -->
+        <div class="flex justify-center items-center relative overflow-hidden rounded-lg p-2 ">
+            <img class="h-[75vh] mb-10" :src="bgUrl">
+        </div>
+
+        <!-- Slider indicators -->
+        <div class="absolute z-30 flex space-x-3  -translate-x-1/2 bottom-5 left-1/2">
+            <button v-for="iml in images.length" :key="iml" id="carousel-indicator-1" type="button"
+                class="w-3 h-3 rounded-full border-white" :class="currentSlide == (iml - 1) ? 'bg-red-700' : 'bg-gray-600'"
+                @click="timerPause = 1; currentSlide = (iml - 1); updateImage()"></button>
+        </div>
+
+        <!-- Slider controls -->
+        <button @click="prevSlide" id="data-carousel-prev" type="button"
+            class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
+            <span
+                class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-orange/30 dark:bg-gray-800/30 group-hover:bg-orange/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg class="w-4 h-4 text-orange-600 dark:text-gray-800" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M5 1 1 5l4 4" />
+                </svg>
+                <span class="hidden">Previous</span>
+            </span>
+        </button>
+
+        <button @click="nextSlide" id="data-carousel-next" type="button"
+            class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
+            <span
+                class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-orange/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg class="w-4 h-4 text-orange-600 dark:text-gray-800" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 9 4-4-4-4" />
+                </svg>
+                <span class="hidden">Next</span>
+            </span>
+        </button>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
